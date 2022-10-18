e642e46c45500bc46a414c71e8335c1e946acd41
kaitlinNicoleFish <kaitlinnicole.fish@student.sl.on.ca>
2022-10-18 14:56:58 -0400

    part 1 of assignment code

--- src/main/java/assignment1/kf/Controllers/MainController.java
+++ src/main/java/assignment1/kf/Controllers/MainController.java
@@ -1,10 +1,18 @@
 package assignment1.kf.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
-// MainController folder used for 
+// MainController folder used for handle GET requests to various pages as we continue to add to this 
+//application 
 @Controller
 public class MainController {
     
+    // Get request code that will retrieve the hotel booking info page
+    @GetMapping("/hotelBookings")
+    public String getHotelBookingRoom()
+    {
+        return "hotelBookings";
+    }
 }
---
+++ src/test/java/assignment1/kf/KfApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment1.kf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
