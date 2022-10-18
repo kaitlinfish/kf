7754b31fa1120418c10b6b1b36fee99a4641f274
kaitlinNicoleFish <kaitlinnicole.fish@student.sl.on.ca>
2022-10-18 14:39:39 -0400

    initial commit

---
+++ src/main/java/assignment1/kf/Controllers/MainController.java
@@ -0,0 +1,10 @@
+package assignment1.kf.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+
+// MainController folder used for 
+@Controller
+public class MainController {
+    
+}
